0d2da391d180e83fbd70d257b5961f7a4d1e86fc
nataliafrancisca <nathifrancisca@gmail.com>
2025-04-11 22:36:35 -0300

    style: fiz a renomeação do arquivo principal

--- src/main/java/br/com/nat/desafioitau/DesafioItauApplication.java
+++ src/main/java/br/com/nat/desafioitau/DesafioItauApplication.java
@@ -1,4 +1,4 @@
-package br.com.nat.desafio_itau;
+package br.com.nat.desafioitau;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/nat/desafioitau/DesafioItauApplicationTests.java
+++ src/test/java/br/com/nat/desafioitau/DesafioItauApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.nat.desafio_itau;
+package br.com.nat.desafioitau;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
