a109689d88686a4a65d2059cfe62c21ac08c4206
nataliafrancisca <nathifrancisca@gmail.com>
2025-04-25 21:55:24 -0300

    feat: adicionei as anotações necessárias para fazer a documentação da API.

--- src/main/java/br/com/nat/desafioitau/controller/EstatisticaController.java
+++ src/main/java/br/com/nat/desafioitau/controller/EstatisticaController.java
@@ -1,6 +1,12 @@
 package br.com.nat.desafioitau.controller;
 
+import br.com.nat.desafioitau.dto.EstatisticaDTO;
 import br.com.nat.desafioitau.service.EstatisticaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("estatistica")
+@Tag(name = "Estatística", description = "Operações relacionadas com estatística.")
 public class EstatisticaController {
     private final EstatisticaService estatisticaService;
 
@@ -18,6 +25,13 @@ public class EstatisticaController {
         this.estatisticaService = estatisticaService;
     }
 
+    @Operation(summary = "Retorna estatística das transações realizadas nos últimos 60 segundos.",
+            description = "Caso não tenha nenhuma transação que se encaixe na regra, os valores retornam com 0")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Estatística gerada com sucesso.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = EstatisticaDTO.class))
+    )
     @GetMapping
     public ResponseEntity<Object> get(){
         return ResponseEntity.status(HttpStatus.OK).body(this.estatisticaService.get());
--- src/main/java/br/com/nat/desafioitau/controller/TransacaoController.java
+++ src/main/java/br/com/nat/desafioitau/controller/TransacaoController.java
@@ -2,6 +2,10 @@ package br.com.nat.desafioitau.controller;
 
 import br.com.nat.desafioitau.model.Transacao;
 import br.com.nat.desafioitau.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transacao")
+@Tag(name = "Transação", description = "Operações relacionadas a transações.")
 public class TransacaoController {
     private final TransacaoService transacaoService;
 
@@ -18,15 +23,24 @@ public class TransacaoController {
         this.transacaoService = transacaoService;
     }
 
+    @Operation(summary = "Adiciona uma nova transação.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Transação criada com sucesso."),
+            @ApiResponse(responseCode = "422", description = "A transação não foi aceita."),
+            @ApiResponse(responseCode = "400", description = "A API não compreendeu a requisição.")
+
+    })
     @PostMapping
     public ResponseEntity<String> create(@Valid @RequestBody Transacao transacao){
         this.transacaoService.adicionar(transacao);
-        return ResponseEntity.status(HttpStatus.CREATED).body("A transação foi aceita com sucesso.");
+        return ResponseEntity.status(HttpStatus.CREATED).body("Transação criada com sucesso.");
     }
 
+    @Operation(summary = "Deleta todas as transações.")
+    @ApiResponse(responseCode = "200", description = "As transações foram deletadas com sucesso.")
     @DeleteMapping
     public ResponseEntity<String> delete(){
         this.transacaoService.deletar();
-        return ResponseEntity.status(HttpStatus.OK).body("Todas as informações foram apagadas com sucesso.");
+        return ResponseEntity.status(HttpStatus.OK).body("Todas as informações foram deletadas com sucesso.");
     }
 }
